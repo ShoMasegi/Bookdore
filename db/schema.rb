ef0489fc8087d7cab19c5bcf07e4157b664931c9
ShoMasegi <m2.b0c27@gmail.com>
2019-05-14 16:27:27 +0900

    🎨 :Change 'Card.status' string to enum

--- db/schema.rb
+++ db/schema.rb
@@ -10,17 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_13_024840) do
+ActiveRecord::Schema.define(version: 2019_05_14_071223) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "cards", force: :cascade do |t|
     t.string "book"
-    t.string "status"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.datetime "deadline"
+    t.string "deadline"
+    t.integer "status", default: 0, null: false
   end
 
 end
