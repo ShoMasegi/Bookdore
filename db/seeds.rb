bdd4b8a08d4a4bc8b185ebf0551a164222b39a55
ShoMasegi <m2.b0c27@gmail.com>
2019-05-16 15:35:39 +0900

    📇 :Fix seed to insert sample data.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create(name: 'Sample User', email: 'sample@example.com', password: 'example', password_confirmation: 'example', admin: false)
+
+30.times do |no|
+  User.create(
+      name: "#{no}Sample User",
+      email: "#{no}sample@example.com",
+      password: "example#{no}",
+      password_confirmation: "example#{no}",
+      admin: false
+  )
+end
