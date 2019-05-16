a5cb2c8666802e87abb0f21e02f831f521628e4c
ShoMasegi <m2.b0c27@gmail.com>
2019-05-16 16:29:21 +0900

    ✨ :Implement login function

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,4 +4,20 @@ class SessionsController < ApplicationController
   def new
   end
 
+  # POST /sessions
+  # POST /sessions.json
+  def create
+    user = User.find_by(email: session_params[:email])
+
+    if user&.authenticate(session_params[:password])
+      session[:user_id] = user.id
+      redirect_to root_path, flash: { success: 'Successfully login!' }
+    end
+  end
+
+  private
+
+    def session_params
+      params.require(:session).permit(:email, :password)
+    end
 end
