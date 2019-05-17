05f70a5788b5ee2f8c07fc25e40c89819c5d099e
ShoMasegi <m2.b0c27@gmail.com>
2019-05-17 11:58:20 +0900

    💾 :Associate cards with a user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,11 @@
 class User < ApplicationRecord
   has_secure_password
 
-  validates :name, presence: true
-  validates :email, presence: true, uniqueness: true
+  validates :name,
+            presence: true
+  validates :email,
+            presence: true,
+            uniqueness: true
+
+  has_many :cards
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_15_072506) do
+ActiveRecord::Schema.define(version: 2019_05_17_024634) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,8 @@ ActiveRecord::Schema.define(version: 2019_05_15_072506) do
     t.datetime "updated_at", null: false
     t.datetime "deadline"
     t.integer "status", default: 0, null: false
+    t.bigint "user_id", null: false
+    t.index ["user_id"], name: "index_cards_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
