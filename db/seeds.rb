c465477d4ce4a860a2cdb7f76846c6fa33ab2b89
ShoMasegi <m2.b0c27@gmail.com>
2019-05-17 12:50:15 +0900

    ⚡️ :Fix seeds to create admin, normal users

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,14 +6,15 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-User.create(name: 'Sample User', email: 'sample@example.com', password: 'example', password_confirmation: 'example', admin: false)
+User.create(name: 'Admin User', email: 'admin@example.com', password: 'admin', password_confirmation: 'admin', admin: true)
+User.create(name: 'Normal User', email: 'normal@example.com', password: 'normal', password_confirmation: 'normal', admin: false)
 
-30.times do |no|
-  User.create(
-      name: "#{no}Sample User",
-      email: "#{no}sample@example.com",
-      password: "example#{no}",
-      password_confirmation: "example#{no}",
-      admin: false
-  )
-end
+# 30.times do |no|
+#   User.create(
+#       name: "#{no}Sample User",
+#       email: "#{no}sample@example.com",
+#       password: "example#{no}",
+#       password_confirmation: "example#{no}",
+#       admin: false
+#   )
+# end
