f218cb380cab1085fa89d3dd911c34788c03e1ad
ShoMasegi <m2.b0c27@gmail.com>
2019-05-21 12:05:16 +0900

    💾 :Exec 'rails g migration add_spent_time_sec_to_cards'"

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_17_024634) do
+ActiveRecord::Schema.define(version: 2019_05_21_025618) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,7 @@ ActiveRecord::Schema.define(version: 2019_05_17_024634) do
     t.datetime "deadline"
     t.integer "status", default: 0, null: false
     t.bigint "user_id", null: false
+    t.integer "spent_time_sec", default: 0, null: false
     t.index ["user_id"], name: "index_cards_on_user_id"
   end
 
