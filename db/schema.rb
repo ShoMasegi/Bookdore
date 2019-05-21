c8db50cc6852a01207affdb02741dd9f9fed5fdb
ShoMasegi <m2.b0c27@gmail.com>
2019-05-21 22:03:49 +0900

    💾 :Exec 'rails g model history'

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_21_113725) do
+ActiveRecord::Schema.define(version: 2019_05_21_125720) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -39,6 +39,11 @@ ActiveRecord::Schema.define(version: 2019_05_21_113725) do
     t.bigint "book_id", null: false
     t.index ["book_id"], name: "index_cards_on_book_id"
     t.index ["user_id"], name: "index_cards_on_user_id"
+  end
+
+  create_table "histories", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "read_histories", force: :cascade do |t|
