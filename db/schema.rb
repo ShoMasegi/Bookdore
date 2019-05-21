3697ae71ce39b562dede407bb4ba5ac262df0fe9
ShoMasegi <m2.b0c27@gmail.com>
2019-05-21 12:08:54 +0900

    💾 :Exec 'rails g migration add_current_page_to_cards'"

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_21_025618) do
+ActiveRecord::Schema.define(version: 2019_05_21_030718) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,7 @@ ActiveRecord::Schema.define(version: 2019_05_21_025618) do
     t.integer "status", default: 0, null: false
     t.bigint "user_id", null: false
     t.integer "spent_time_sec", default: 0, null: false
+    t.integer "current_page", default: 0, null: false
     t.index ["user_id"], name: "index_cards_on_user_id"
   end
 
