e05ed5aa07d6a822343a9a38da3717563156ec42
ShoMasegi <m2.b0c27@gmail.com>
2019-05-22 21:07:26 +0900

    🚑 :Remove unnecessary validation from card

--- app/models/card.rb
+++ app/models/card.rb
@@ -1,12 +1,6 @@
 class Card < ApplicationRecord
   enum status: [:want, :bought, :reading, :read]
-  validates :book,
-            presence: true,
-            length: { maximum: 50 },
-            uniqueness: true
-  validates :status,
-            presence: true,
-            length: { maximum: 100 }
+
   validates :deadline,
             presence: true
 
