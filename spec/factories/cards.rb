2e5d1ad4bd7007cd384b9ab0ac1c0a8cc90fe35a
ShoMasegi <m2.b0c27@gmail.com>
2019-05-27 10:21:58 +0900

    🚨 :Create 'History' model test

--- spec/factories/cards.rb
+++ spec/factories/cards.rb
@@ -1,6 +1,10 @@
 FactoryBot.define do
   factory :card do
-    book { "Test Book" }
-    status { "Test Status" }
+    deadline { '2019-10-20 20:00:00.000000' }
+    status { 2 }
+    association :user
+    association :book
+    spent_time_sec { 600000 }
+    current_page { 230 }
   end
 end
---
+++ spec/factories/read_histories.rb
@@ -0,0 +1,6 @@
+FactoryBot.define do
+  factory :read_history do
+    page_count { 20 }
+    time_sec { 1500 }
+  end
+end
